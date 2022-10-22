bb22072d7537dcdbca019ee58c18bc483bdcb366
Sujeong Park <krystarline@gmail.com>
2022-10-22 17:53:56 +0900

    🐛 fix(modal): update flickering when switching

--- components/ModalGlobal.tsx
+++ components/ModalGlobal.tsx
@@ -1,28 +1,37 @@
-import { useRecoilState } from "recoil";
+import { Modal } from "@mui/material";
+import { useRecoilValue } from "recoil";
+import useModal from "../lib/hooks/useModal";
+// import { modalState } from "../recoil/modal";
 import ModalBuild from "./ModalBuild";
 import ModalCreate from "./ModalCreate";
 
 import { modalState } from "../lib/recoil/modal";
 
 function ModalGlobal() {
-  const { modalType } = useRecoilState(modalState)[0] || {};
+  const { modalType } = useRecoilValue(modalState) || {};
+  const { isModal, hideModal } = useModal();
 
   const renderComponent = () => {
-    if (!modalType) {
-      return null;
-    }
-
     switch (modalType) {
       case "ModalCreate":
         return <ModalCreate />;
       case "ModalBuild":
         return <ModalBuild />;
       default:
-        return null;
+        return <div />;
     }
   };
 
-  return <>{renderComponent()}</>;
+  return (
+    <Modal
+      open={isModal != null}
+      onClose={() => hideModal()}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      {renderComponent()}
+    </Modal>
+  );
 }
 
 export default ModalGlobal;
