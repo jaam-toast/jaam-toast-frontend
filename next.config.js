02ad9d0a61afcf1756d2506ed69f2c9bb59df029
Taewan Lim <taewan.seoul@gmail.com>
2022-10-22 21:04:25 +0900

    ➕ chore: modify eslint and config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
