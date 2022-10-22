6a631a4db835757b10d012ccea8caea9666fea47
Taewan Lim <taewan.seoul@gmail.com>
2022-10-22 21:15:02 +0900

    ✨ feat: set up next/router and optional rendering depending on login state

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import ButtonCreate from "../components/ButtonCreate";
@@ -5,26 +9,44 @@ import Content from "../components/Content";
 import NavBar from "../components/Navbar";
 import SearchInput from "../components/SearchInput";
 
+import Login from "./login";
+import loginState from "../lib/recoil/auth";
+
 function Dashboard() {
+  const isLoggedIn = useRecoilValue(loginState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace("/login");
+    }
+  }, [isLoggedIn, router]);
+
   return (
-    <>
-      <NavBar />
-      <Container maxWidth="lg">
-        <Box
-          display="flex"
-          sx={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            m: 1,
-          }}
-        >
-          <SearchInput />
-          <ButtonCreate />
-        </Box>
-        <Content />
-      </Container>
-    </>
+    <Container>
+      {isLoggedIn ? (
+        <>
+          <NavBar />
+          <Container maxWidth="lg">
+            <Box
+              display="flex"
+              sx={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                m: 1,
+              }}
+            >
+              <SearchInput />
+              <ButtonCreate />
+            </Box>
+            <Content />
+          </Container>
+        </>
+      ) : (
+        <Login />
+      )}
+    </Container>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,46 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import ButtonLogin from "../components/ButtonLogin";
 
+import Dashboard from "./dashboard";
+import loginState from "../lib/recoil/auth";
+
 function PageLanding() {
+  const isLoggedIn = useRecoilValue(loginState);
+  const router = useRouter();
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace("/dashboard");
+    }
+
+    setIsSSR(false);
+  }, [isLoggedIn, router]);
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        display="flex"
-        sx={{
-          justifyContent: "center",
-          verticalAlign: "middle",
-          alignItems: "center",
-          m: 1,
-        }}
-      >
-        <ButtonLogin />
-      </Box>
+    <Container>
+      {!isSSR && isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        <Container maxWidth="lg">
+          <Box
+            display="flex"
+            sx={{
+              justifyContent: "center",
+              verticalAlign: "middle",
+              alignItems: "center",
+              m: 1,
+            }}
+          >
+            <ButtonLogin />
+          </Box>
+        </Container>
+      )}
     </Container>
   );
 }
