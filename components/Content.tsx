6e627c2988be67b0887edc6e90b669e008a9925b
Sujeong Park <krystarline@gmail.com>
2022-11-03 19:25:39 +0900

    ✨ feat: implement deploy card layout

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,7 +1,10 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import ChangeHistoryIcon from "@mui/icons-material/ChangeHistory";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
 function Content() {
   return (
@@ -17,10 +20,31 @@ function Content() {
 }
 
 const card = (
-  <>
-    <CardActions />
-    <CardContent />
-  </>
+  <CardActionArea>
+    <CardContent sx={{ padding: 0, margin: 2 }}>
+      <Box display="flex" sx={{ flexDirection: "row", alignItems: "center" }}>
+        <ChangeHistoryIcon fontSize="medium" />
+        <Box display="flex" sx={{ flexDirection: "column", marginLeft: 1 }}>
+          <Typography fontSize="large" fontWeight="bold">
+            repository-name
+          </Typography>
+          <Typography fontSize="medium">site-url</Typography>
+        </Box>
+      </Box>
+      <Typography fontSize="small" fontWeight="medium" sx={{ marginTop: 2 }}>
+        📝[docs] Update README
+      </Typography>
+      <Box
+        display="flex"
+        sx={{ flexDirection: "row", marginTop: 2, alignItems: "center" }}
+      >
+        <Typography fontSize="small" fontWeight="medium">
+          22h ago via
+        </Typography>
+        <GitHubIcon sx={{ marginLeft: 0.5 }} />
+      </Box>
+    </CardContent>
+  </CardActionArea>
 );
 
 export default Content;
