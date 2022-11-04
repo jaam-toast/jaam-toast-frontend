f0291ed19aaef16f246b85386ca0e8ad95ad7e2f
Sujeong Park <krystarline@gmail.com>
2022-11-04 18:44:55 +0900

    ♻️  refactor: refactor cardlist using map method

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-array-index-key */
+import { useState } from "react";
+
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -6,45 +9,58 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 
-function Content() {
+function RepoCard() {
   return (
-    <Box
-      sx={{
-        m: 1,
-        width: "25%",
-      }}
-    >
-      <Card variant="elevation">{card}</Card>
-    </Box>
+    <CardActionArea>
+      <CardContent sx={{ padding: 0, margin: 2 }}>
+        <Box display="flex" sx={{ flexDirection: "row", alignItems: "center" }}>
+          <ChangeHistoryIcon fontSize="medium" />
+          <Box display="flex" sx={{ flexDirection: "column", marginLeft: 1 }}>
+            <Typography fontSize="large" fontWeight="bold">
+              repository-name
+            </Typography>
+            <Typography fontSize="medium">site-url</Typography>
+          </Box>
+        </Box>
+        <Typography fontSize="small" fontWeight="medium" sx={{ marginTop: 2 }}>
+          📝[docs] Update README
+        </Typography>
+        <Box
+          display="flex"
+          sx={{ flexDirection: "row", marginTop: 2, alignItems: "center" }}
+        >
+          <Typography fontSize="small" fontWeight="medium">
+            22h ago via
+          </Typography>
+          <GitHubIcon fontSize="small" sx={{ marginLeft: 0.5 }} />
+        </Box>
+      </CardContent>
+    </CardActionArea>
   );
 }
 
-const card = (
-  <CardActionArea>
-    <CardContent sx={{ padding: 0, margin: 2 }}>
-      <Box display="flex" sx={{ flexDirection: "row", alignItems: "center" }}>
-        <ChangeHistoryIcon fontSize="medium" />
-        <Box display="flex" sx={{ flexDirection: "column", marginLeft: 1 }}>
-          <Typography fontSize="large" fontWeight="bold">
-            repository-name
-          </Typography>
-          <Typography fontSize="medium">site-url</Typography>
-        </Box>
-      </Box>
-      <Typography fontSize="small" fontWeight="medium" sx={{ marginTop: 2 }}>
-        📝[docs] Update README
-      </Typography>
-      <Box
-        display="flex"
-        sx={{ flexDirection: "row", marginTop: 2, alignItems: "center" }}
-      >
-        <Typography fontSize="small" fontWeight="medium">
-          22h ago via
-        </Typography>
-        <GitHubIcon sx={{ marginLeft: 0.5 }} />
-      </Box>
-    </CardContent>
-  </CardActionArea>
-);
+function Content() {
+  const [cardList] = useState([]);
+
+  return (
+    <Box display="flex" sx={{ flexDirection: "row" }}>
+      {cardList.map((card, index) => {
+        return (
+          <Box
+            key={index}
+            sx={{
+              padding: 1,
+              margin: 1,
+            }}
+          >
+            <Card variant="elevation">
+              <RepoCard />
+            </Card>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+}
 
 export default Content;
