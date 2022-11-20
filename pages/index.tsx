1c7e6950ce1a7c5bca2731af960cd411d242e499
Sujeong Park <krystarline@gmail.com>
2022-11-20 17:01:01 +0900

    ✨ feat(landing page): implement landing page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import { useRecoilValue } from "recoil";
 
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import ButtonLogin from "../components/ButtonLogin";
 
+import { Typography } from "@mui/material";
+import NavBar from "../components/Navbar";
 import Dashboard from "./dashboard";
+import ButtonLogin from "../components/ButtonLogin";
 import { isLoggedInState } from "../lib/recoil/auth";
 
 function PageLanding() {
@@ -23,23 +25,64 @@ function PageLanding() {
   }, [isLoggedIn, router]);
 
   return (
-    <Container>
+    <Container maxWidth={false} disableGutters>
       {!isSSR && isLoggedIn ? (
         <Dashboard />
       ) : (
-        <Container maxWidth="lg">
+        <>
+          <NavBar />
           <Box
+            component="div"
             display="flex"
             sx={{
+              flexDirection: "column",
               justifyContent: "center",
               verticalAlign: "middle",
               alignItems: "center",
-              m: 1,
             }}
           >
-            <ButtonLogin />
+            <Box
+              component="img"
+              position="relative"
+              src="/landingpage_content.png"
+              alt="jaam-toast"
+              sx={{
+                width: "100%",
+                height: "calc(100vh - 64px)",
+                maxWidth: { xs: "100%", md: "100%" },
+                maxHeight: {
+                  xs: "calc(100vh - 64px)",
+                  md: "calc(100vh - 64px)",
+                },
+              }}
+            />
+            <Typography
+              position="absolute"
+              variant="h2"
+              sx={{
+                padding: 1,
+                height: "70vh",
+                fontWeight: "bold",
+              }}
+            >
+              Deploy your own project.
+            </Typography>
+            <Typography
+              position="absolute"
+              variant="body1"
+              sx={{
+                padding: 1,
+                height: "45vh",
+                fontColor: "#d3d3d3",
+              }}
+            >
+              Every deployment from the 2022 edition of <b>jaam-toast.</b>
+            </Typography>
+            <Box position="absolute" sx={{ padding: 1, marginTop: 40 }}>
+              <ButtonLogin />
+            </Box>
           </Box>
-        </Container>
+        </>
       )}
     </Container>
   );
