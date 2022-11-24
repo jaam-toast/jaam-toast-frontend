8f00969bcce1fa4b791c9aed7abb242e7cfd4da0
Sujeong Park <krystarline@gmail.com>
2022-11-24 12:19:45 +0900

    ✨ feat: refactor landing and login page layout 

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,25 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
 import { setCookie } from "cookies-next";
 
 import Container from "@mui/material/Container";
 
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 import { getUserDeployments, login } from "../lib/api";
 import loginState from "../lib/recoil/auth";
 import userDeploymentsState from "../lib/recoil/userDeployments";
 
 import ButtonLogin from "../components/ButtonLogin";
+import NavBar from "../components/Navbar";
 
 import { LoginData, UserDeploymentData } from "../types";
 
 function Login() {
+  const [isSSR, setIsSSR] = useState(true);
   const setLoginState = useSetRecoilState<LoginData | null>(loginState);
   const setUserDeploymentsState =
     useSetRecoilState<UserDeploymentData[]>(userDeploymentsState);
 
   const router = useRouter();
   const authCode = router.query.code;
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
 
   useEffect(() => {
     const handleLogin = async (code: string) => {
@@ -56,17 +65,32 @@ function Login() {
   }, [authCode, router, setLoginState, setUserDeploymentsState]);
 
   return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <ButtonLogin />
+    <Container maxWidth={false} disableGutters>
+      {!isSSR ? (
+        <>
+          <NavBar />
+          <Divider />
+          <Box
+            component="div"
+            display="flex"
+            sx={{
+              padding: 15,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              Log in to Vercel
+            </Typography>
+            <Box sx={{ padding: 5 }}>
+              <ButtonLogin />
+              <Divider sx={{ padding: 1 }} />
+            </Box>
+          </Box>
+          <Divider sx={{ marginTop: 20 }} />
+        </>
+      ) : null}
     </Container>
   );
 }
