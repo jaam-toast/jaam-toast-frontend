ae053e818388b6ffc33e558bf28f448a2893ad66
Sujeong Park <krystarline@gmail.com>
2022-11-25 20:24:57 +0900

    ✨ feat: implement mobiledefense for mobile browser

--- components/MobileDefense.tsx
+++ components/MobileDefense.tsx
@@ -1,57 +1,51 @@
-import useMediaQuery from "@mui/material/useMediaQuery";
-
 import { Container, Box, IconButton, Typography } from "@mui/material";
 import { ChangeHistory as ChangeHistoryIcon } from "@mui/icons-material";
 
 function MobileDefense() {
-  const isMobile = useMediaQuery("(max-width: 1000px)");
-
   return (
     <Container fixed sx={{ height: "10vh", padding: 1 }}>
-      {isMobile ? (
+      <Box
+        display="flex"
+        sx={{
+          ...style,
+        }}
+      >
         <Box
           display="flex"
           sx={{
-            ...style,
+            flexDirection: "row",
+            padding: 1,
+            paddingBottom: 0,
+            margin: 1,
           }}
         >
-          <Box
-            display="flex"
-            sx={{
-              flexDirection: "row",
-              padding: 1,
-              paddingBottom: 0,
-              margin: 1,
-            }}
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="brand-logo"
           >
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="brand-logo"
-            >
-              <ChangeHistoryIcon fontSize="large" />
-            </IconButton>
-            <Typography variant="h6" sx={{ padding: 2, fontWeight: "bold" }}>
-              Jaam-Toast
-            </Typography>
-          </Box>
-          <Box
-            display="flex"
-            sx={{
-              flexDirection: "column",
-              padding: 1,
-              paddingTop: 0,
-              margin: 1,
-            }}
-          >
-            <Typography>Not supported in mobile environment.</Typography>
-            <Typography>
-              <b>Please run it in a desktop environment.</b>
-            </Typography>
-          </Box>
+            <ChangeHistoryIcon fontSize="large" />
+          </IconButton>
+          <Typography variant="h6" sx={{ padding: 2, fontWeight: "bold" }}>
+            Jaam-Toast
+          </Typography>
         </Box>
-      ) : null}
+        <Box
+          display="flex"
+          sx={{
+            flexDirection: "column",
+            padding: 1,
+            paddingTop: 0,
+            margin: 1,
+          }}
+        >
+          <Typography>Not supported in mobile environment.</Typography>
+          <Typography>
+            <b>Please run it in a desktop environment.</b>
+          </Typography>
+        </Box>
+      </Box>
     </Container>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
-import { ThemeProvider, CssBaseline, useMediaQuery } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+
+import { isMobile } from "react-device-detect";
 
 import MobileDefense from "../components/MobileDefense";
 import ModalGlobal from "../components/ModalGlobal";
@@ -9,20 +12,30 @@ import theme from "../utils/theme";
 import "../public/fonts/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isMobile = useMediaQuery("(max-width: 1000px)");
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  const renderItems = () => {
+    if (isMobile) {
+      return <MobileDefense />;
+    }
+
+    return (
+      <>
+        <ModalGlobal />
+        <Component {...pageProps} />
+      </>
+    );
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <RecoilRoot>
         <CssBaseline />
-        {isMobile ? (
-          <MobileDefense />
-        ) : (
-          <>
-            <ModalGlobal />
-            <Component {...pageProps} />
-          </>
-        )}
+        {!isSSR ? <>{renderItems()}</> : null}
       </RecoilRoot>
     </ThemeProvider>
   );
