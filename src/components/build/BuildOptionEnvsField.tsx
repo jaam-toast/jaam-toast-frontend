7bf62dca3ca6a105c8d54ed98eef8a8f08e35d0a
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-25 19:47:56 +0900

    ♻️ refactor: refactorying build options page

--- src/components/build/BuildOptionEnvsField.tsx
+++ src/components/build/BuildOptionEnvsField.tsx
@@ -1,27 +1,133 @@
-import { AccordionDetails, Divider } from "@mui/material";
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
+import { Container, Divider, IconButton, TextField } from "@mui/material";
+import {
+  ExpandMore as ExpandMoreIcon,
+  Add as AddIcon,
+  Remove as RemoveIcon,
+} from "@mui/icons-material";
 import { Box } from "@mui/system";
 
-import BuildOptionEnvsTextField from "./BuildOptionEnvsTextField";
-import BuildOptionEnvsSavedTextField from "./BuildOptionEnvsSavedTextField";
+import { BLACK, GREY } from "src/constants/colors";
 
-function BuildOptionEnvsField() {
-  // TODO: existed env list styling(****).
-  const envList = [{ key: "example env key 1", value: "example env value 2" }];
+import type { Env } from "types/projectOption";
+
+type BuildOptionEnvsFieldProps = {
+  defaultEnvs?: Env[];
+  onEnvsChange: (envs: Env[]) => void;
+};
+
+function BuildOptionEnvsField({
+  defaultEnvs = [],
+  onEnvsChange,
+}: BuildOptionEnvsFieldProps) {
+  const [envKey, setEnvKey] = useState<string>("");
+  const [envValue, setEnvValue] = useState<string>("");
+  const [envs, setEnvs] = useState<Env[]>(defaultEnvs);
+
+  const handleAddEnv = () => {
+    if (!envKey || !envValue) {
+      return;
+    }
+
+    const newEnvs = envs.concat({ key: envKey, value: envValue });
+
+    setEnvs(newEnvs);
+    onEnvsChange(newEnvs);
+    setEnvKey("");
+    setEnvValue("");
+  };
+
+  const handleEnvFieldKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter") {
+      return;
+    }
+
+    handleAddEnv();
+  };
+
+  const handleRemoveEnv = (envIdx: number) => {
+    const newEnvs = envs.filter((env: Env, idx: number) => idx !== envIdx);
+
+    setEnvs(newEnvs);
+    onEnvsChange(newEnvs);
+  };
 
   return (
-    <AccordionDetails sx={{ mt: 1 }}>
-      <BuildOptionEnvsTextField />
-      <Divider sx={{ marginTop: 2.5, marginBottom: 2.5 }} />
-      {envList.map((env, index) => (
-        <Box
-          key={`${env.key}-${index}`}
-          display="flex"
-          sx={{ flexDirection: "row", marginBottom: 1.5 }}
-        >
-          <BuildOptionEnvsSavedTextField envIndex={index} envsState={envList} />
-        </Box>
-      ))}
-    </AccordionDetails>
+    <Container disableGutters>
+      <Box
+        display="flex"
+        sx={{ flexDirection: "row", marginBottom: 1.5, gap: "0.5rem" }}
+      >
+        <TextField
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEnvKey(e.target.value)
+          }
+          onKeyDown={handleEnvFieldKeyPress}
+          placeholder="Key"
+          value={envKey}
+          size="small"
+          sx={{ fontSize: "small", width: "100%", marginTop: 1.5 }}
+        />
+        <TextField
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEnvValue(e.target.value)
+          }
+          onKeyDown={handleEnvFieldKeyPress}
+          placeholder="Value"
+          value={envValue}
+          size="small"
+          sx={{ fontSize: "small", width: "100%", marginTop: 1.5 }}
+        />
+        <IconButton onClick={handleAddEnv} sx={{ marginTop: 1 }}>
+          <AddIcon
+            sx={{
+              padding: "0",
+              color: GREY,
+              ":hover": {
+                color: BLACK,
+              },
+            }}
+          />
+        </IconButton>
+      </Box>
+
+      {envs.length > 0 && <Divider sx={{ margin: "1rem 0" }} />}
+
+      {envs
+        .slice()
+        .reverse()
+        .map((env, idx, arr) => (
+          <Box
+            key={`${env.key}-${idx}`}
+            display="flex"
+            sx={{ flexDirection: "row", marginBottom: 1.5, gap: "0.5rem" }}
+          >
+            <TextField
+              value={env.key}
+              size="small"
+              disabled
+              sx={{ fontSize: "small", width: "100%", marginTop: 0.5 }}
+            />
+            <TextField
+              value={env.value}
+              size="small"
+              disabled
+              sx={{ fontSize: "small", width: "100%", marginTop: 0.5 }}
+            />
+            <IconButton onClick={() => handleRemoveEnv(arr.length - idx - 1)}>
+              <RemoveIcon
+                sx={{
+                  padding: "0",
+                  color: GREY,
+                  ":hover": {
+                    color: BLACK,
+                  },
+                }}
+              />
+            </IconButton>
+          </Box>
+        ))}
+    </Container>
   );
 }
 
