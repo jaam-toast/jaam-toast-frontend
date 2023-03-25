686fe83b25f093a8845c9bc5dbc66e93801b6417
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-25 17:14:48 +0900

    ♻️ refactor: apply TextField debounce

--- src/components/@shared/TextField.tsx
+++ src/components/@shared/TextField.tsx
@@ -1,10 +1,40 @@
-import { TextField as TextFieldMui } from "@mui/material";
-import { TextFieldProps } from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import {
+  TextField as TextFieldMui,
+  TextFieldProps as TextFieldMuiProps,
+} from "@mui/material";
 
-function TextField({ ...props }: TextFieldProps) {
+import useDebounce from "src/hooks/useDebounce";
+
+type TextFieldProps = Partial<TextFieldMuiProps> & {
+  delay?: number;
+  defaultValue?: string;
+  onTextFieldChange?: (text: string) => void;
+};
+
+function TextField({
+  delay = 1000,
+  defaultValue = "",
+  onTextFieldChange,
+  ...props
+}: TextFieldProps) {
+  const [text, setText] = useState<string>(defaultValue);
+
+  const onTextFieldDebounceChange = useDebounce((text: string) => {
+    onTextFieldChange(text);
+  }, delay);
+
+  const handleMuiTextFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
+    setText(text);
+    onTextFieldDebounceChange(text);
+  };
+
   return (
     <TextFieldMui
       id="outlined-basic"
+      value={text}
+      onChange={handleMuiTextFieldChange}
       variant="outlined"
       autoComplete="off"
       sx={{
