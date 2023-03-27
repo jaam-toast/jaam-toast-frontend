29387eb70e3e2d377c56d3c12cc7844d35ab3504
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-27 13:25:19 +0900

    💄 style: NavNar styling

--- src/components/@shared/Navbar.tsx
+++ src/components/@shared/Navbar.tsx
@@ -1,10 +1,18 @@
 import { useRouter } from "next/router";
-import { AppBar, Toolbar, Typography, Tooltip } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Tooltip,
+  SvgIcon,
+  Container,
+} from "@mui/material";
 import CallMadeIcon from "@mui/icons-material/CallMade";
 
 import { Button } from ".";
 import useUser from "src/hooks/useUser";
 import { BLACK, WHITE } from "src/constants/colors";
+import Image from "next/image";
 
 function NavBar() {
   const { isLoggedIn, logout } = useUser();
@@ -15,59 +23,67 @@ function NavBar() {
   };
 
   return (
-    <AppBar
-      position="relative"
-      color="inherit"
-      elevation={0}
-      sx={{
-        height: "7vh",
-        justifyContent: "center",
-        position: "sticky",
-        padding: "0 10rem",
-        borderBottom: "1px solid #1D1D1D",
-      }}
-    >
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography
-          variant="h6"
-          component="div"
+    <Container disableGutters>
+      <AppBar
+        position="relative"
+        color="inherit"
+        elevation={0}
+        sx={{
+          height: "7vh",
+          justifyContent: "center",
+          position: "sticky",
+        }}
+      >
+        <Toolbar
           sx={{
-            flexGrow: 1,
-            fontWeight: "700",
-            fontSize: "1.5rem",
-            fontFamily: "GmarketSans",
-            letterSpacing: "-2px",
+            justifyContent: "space-between",
           }}
         >
-          / Jaam Toast
-        </Typography>
-        {isLoggedIn && (
-          <Tooltip title="Log out">
-            <Button
-              variant="dark"
-              size="small"
-              color="light"
-              sx={{
-                fontSize: "0.9rem",
-                color: BLACK,
-                padding: "0.4rem 1.5rem",
-                border: `1px solid ${BLACK}`,
-                backgroundColor: WHITE,
-                ":hover": {
-                  color: WHITE,
-                  backgroundColor: BLACK,
-                  border: `1px solid ${WHITE}`,
-                },
-              }}
-              onClick={logout}
-            >
-              Log out
-              <CallMadeIcon sx={{ marginLeft: "0.5rem" }} />
-            </Button>
-          </Tooltip>
-        )}
-      </Toolbar>
-    </AppBar>
+          <Typography
+            variant="button"
+            sx={{
+              display: "inline",
+              width: "12rem",
+              textTransform: "capitalize",
+              fontWeight: "700",
+              fontSize: "1.5rem",
+              fontFamily: "GmarketSans",
+              letterSpacing: "-2px",
+              userSelect: "none",
+              cursor: "pointer",
+            }}
+            onClick={handleLogoClick}
+          >
+            / Jaam Toast
+          </Typography>
+          {isLoggedIn && (
+            <Tooltip title="Log out">
+              <Button
+                variant="dark"
+                size="small"
+                color="light"
+                sx={{
+                  fontSize: "0.9rem",
+                  color: BLACK,
+                  padding: "0.4rem 1.5rem",
+                  border: `1px solid ${BLACK}`,
+                  backgroundColor: WHITE,
+                  ":hover": {
+                    color: WHITE,
+                    backgroundColor: BLACK,
+                    border: `1px solid ${WHITE}`,
+                  },
+                }}
+                onClick={logout}
+              >
+                Log out
+                <CallMadeIcon sx={{ marginLeft: "0.5rem" }} />
+              </Button>
+            </Tooltip>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
 
