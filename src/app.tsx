8d69a6900952b85f898e8c698186e22d1bda94bb
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-04-11 16:21:11 +0900

    ♻️ refactor: add suspense

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,34 @@
 import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
 import { Header } from "./@shared/Header";
 import { Landing } from "./Landing";
 import { ProjectList } from "./ProjectList";
 import { RepositorySelect } from "./RepositorySelect";
+import { BuildOptionSelect } from "./BuildOptionSelect";
 import * as css from "./app.css";
 
 export function App() {
   return (
     <div className={css.container}>
       <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/projects" element={<ProjectList />} />
-        {/* <Route path="/new/:userName/:projectName" element={<RepositoryImport />} /> */}
-        <Route path="/new/:userName" element={<RepositorySelect />} />
-        {/* <Route path="/new/:userName/:repository" element={<RepositoryImport />} /> */}
-        {/* <Route path="/new/:userName/:repository/deploy" element={<RepositoryImport />} /> */}
-        {/* <Route path="/new/:userName/:repository/preview" element={<RepositoryImport />} /> */}
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/projects" element={<ProjectList />} />
+          {/* <Route path="/new/:userName/:projectName" element={<ProjectDetail />} /> */}
+          <Route path="/new/:userName" element={<RepositorySelect />} />
+          <Route
+            path="/new/:userName/:repository"
+            element={<BuildOptionSelect />}
+          />
+          {/* <Route
+            path="/new/:userName/:repository/deploy"
+            element={<ProjectDeploy />}
+          /> */}
+          {/* <Route path="/new/:userName/:repository/preview" element={<ProejctPreview />} /> */}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
