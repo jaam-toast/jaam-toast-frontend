ef9db2df1b5ed0cb596aaf4a474794218900d4a8
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-04-11 10:24:49 +0900

    💄 style: styling buildStepCard

---
+++ src/@shared/Avatar.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from "react";
+import * as css from "./Avatar.css";
+
+export function Avatar({
+  children,
+  size = 4,
+  active = false,
+}: {
+  children: ReactElement | string;
+  size: number;
+  active: boolean;
+}) {
+  return (
+    <div className={`${css.container} ${active ? css.active : css.notActive}`}>
+      {children}
+    </div>
+  );
+}
