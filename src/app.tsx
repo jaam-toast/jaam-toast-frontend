982c5993ef4bd080fe0cd2e1a914097b158a3219
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-04-12 16:50:21 +0900

    🐛 fix: fix redirect codes

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 
 import { Header } from "./@shared/Header";
@@ -6,27 +6,34 @@ import { Landing } from "./Landing";
 import { ProjectList } from "./ProjectList";
 import { RepositorySelect } from "./RepositorySelect";
 import { BuildOptionSelect } from "./BuildOptionSelect";
+import { ProjectDeploy } from "./ProjectDeploy";
+import { ProjectDetail } from "./ProjectDetail";
+import { useAuth } from "./@shared";
 import * as css from "./app.css";
 
 export function App() {
+  const { user } = useAuth();
+
   return (
     <div className={css.container}>
       <Header />
       <Suspense>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route
+            path="/"
+            element={user ? <Navigate to="/projects" /> : <Landing />}
+          />
           <Route path="/projects" element={<ProjectList />} />
-          {/* <Route path="/new/:userName/:projectName" element={<ProjectDetail />} /> */}
           <Route path="/new/:userName" element={<RepositorySelect />} />
           <Route
             path="/new/:userName/:repository"
             element={<BuildOptionSelect />}
           />
-          {/* <Route
+          <Route
             path="/new/:userName/:repository/deploy"
             element={<ProjectDeploy />}
-          /> */}
-          {/* <Route path="/new/:userName/:repository/preview" element={<ProejctPreview />} /> */}
+          />
+          <Route path="/:userName/:projectName" element={<ProjectDetail />} />
         </Routes>
       </Suspense>
     </div>
