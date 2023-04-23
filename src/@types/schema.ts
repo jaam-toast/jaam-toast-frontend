a04d6f73d1009e113216de8d299c4b700f5633d3
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-24 05:02:26 +0900

    ✨ feat: create schema api and type

---
+++ src/@types/schema.ts
@@ -0,0 +1,31 @@
+type Properties = {
+  type: string;
+  minLength?: number;
+  maxLength?: number;
+  minimum?: number;
+  maximum?: number;
+  min?: number;
+  max?: number;
+};
+
+export type Schema = {
+  title: string;
+  description?: string;
+  type?: "object";
+  properties: {
+    [propertyName in string]: Properties;
+  };
+  required?: string[];
+};
+
+export const SCHEMA_FIELD_TYPE: string[] = [
+  "text",
+  "textarea",
+  "email",
+  "link",
+  "date",
+  "number",
+  "boolean",
+];
+
+export type SchemaFieldType = typeof SCHEMA_FIELD_TYPE[number];
