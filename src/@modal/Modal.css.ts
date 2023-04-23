4b6435c9918e6897f5b4bb2d367495158c7b5b32
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-24 04:45:59 +0900

    ✨ feat: create modal and useModal hook

---
+++ src/@modal/Modal.css.ts
@@ -0,0 +1,64 @@
+import { keyframes, style, styleVariants } from "@vanilla-extract/css";
+import { s } from "../@styles";
+import * as colors from "../@config/colors";
+import { fullHeigth } from "src/@styles/styles.css";
+
+/**
+ * modal style variants and keyframes
+ */
+const fadeInKeyframe = keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
+
+const fadeOutKeyframe = keyframes({
+  from: { opacity: 1 },
+  to: { opacity: 0 },
+});
+
+const slideToLeftKeyframe = keyframes({
+  from: { transform: "translateX(100%)", opacity: 0 },
+  to: { transform: "translateX(0%)", opacity: 1 },
+});
+
+export const animation = styleVariants({
+  none: {},
+  slideToLeft: {
+    animation: `${slideToLeftKeyframe} 0.3s`,
+  },
+});
+
+export const location = styleVariants({
+  center: {},
+  right: { justifyContent: "flex-end" },
+});
+
+/**
+ * classes
+ */
+export const container = style([
+  s.flex,
+  s.alignItemsCenter,
+  s.justifyContentCenter,
+  s.full,
+  {
+    position: "fixed",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    transition: "opacity 3s",
+    animation: `${fadeInKeyframe} 0.3s`,
+  },
+]);
+
+export const modal = style([
+  s.flex,
+  s.alignItemsCenter,
+  fullHeigth,
+  s.scroll,
+  {
+    transition: "all 0.5s",
+  },
+]);
