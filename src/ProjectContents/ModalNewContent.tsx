96c4eb39e7b2d04277e2da8d28994ab06905a32e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-27 23:31:43 +0900

    ✨ feat: create project contents modal and contents list

---
+++ src/ProjectContents/ModalNewContent.tsx
@@ -0,0 +1,180 @@
+import { useEffect, useMemo, useState } from "react";
+import dayjs from "dayjs";
+import jsonSchemaToJaamSchema from "../@packages/json-schema-to-jaam-schema";
+
+import { SelectBox, TextField, useModal } from "../@shared";
+import { FieldTitle } from "../ProjectSchema/FieldTitle";
+import { useCreateSchemaMutation } from "../ProjectSchema/useSchemaMutation";
+import * as css from "../ProjectSchema/ModalNewSchema.css";
+
+import { SchemaPropertyType } from "../@packages/json-schema-to-jaam-schema/types";
+import type { SchemaList } from "../@types/api";
+import { useContentsState, useSetContentsState } from "./useContentsStore";
+import { Navigate } from "react-router-dom";
+import jsonSchemaValidator from "../@packages/json-schema-validator";
+import { useCreateContentMutation } from "./useContentsMutation";
+
+type Options = {
+  token: string;
+  schemaList?: SchemaList[];
+};
+
+type EditorType = "textfield" | "textarea" | "dateInput";
+
+const editorForType: Record<SchemaPropertyType, EditorType> = {
+  text: "textfield",
+  textarea: "textarea",
+  email: "textfield",
+  link: "textfield",
+  number: "textfield",
+  date: "dateInput",
+  boolean: "textfield",
+};
+
+export function ModalNewContent({ token, schemaList }: Options) {
+  const [currentSchemaIndex, setCurrentSchemaIndex] = useState<number>(0);
+  const { content, contentsError } = useContentsState();
+  const { setContents } = useSetContentsState();
+  const { closeModal } = useModal();
+
+  if (!schemaList) {
+    return <Navigate to="/" />;
+  }
+
+  const { schema } = useMemo(
+    () => schemaList[currentSchemaIndex],
+    [currentSchemaIndex],
+  );
+  const schemaProperty = useMemo(
+    () => Object.entries(jsonSchemaToJaamSchema(schema).properties),
+    [currentSchemaIndex],
+  );
+
+  useEffect(() => {
+    schemaProperty.forEach(([name, data]) => {
+      if (data.type === "date") {
+        setContents({ name, content: dayjs().format("YYYY-MM-DD"), schema });
+      }
+    });
+  }, [currentSchemaIndex]);
+
+  const createContent = useCreateContentMutation({
+    onSuccess: () => {
+      alert("Success content creation");
+      closeModal();
+    },
+    onError: () => {
+      alert("Failed to create schema. Please try again.");
+    },
+  });
+
+  const handleClickSave = () => {
+    const { result, message } = jsonSchemaValidator({
+      schema,
+      content,
+    });
+
+    if (!result) {
+      return alert(message);
+    }
+
+    createContent.mutate({ token, schemaName: schema.title });
+  };
+
+  return (
+    <div className={css.container}>
+      <header className={css.header}>
+        <div className={css.headerFirstLine}>
+          <h3>
+            Create new Content
+            {/* <span className={css.schemaName}> {schema.title}</span> */}
+          </h3>
+        </div>
+      </header>
+      <section className={css.schemaNameSection}>
+        <div>
+          <FieldTitle>Schema Type</FieldTitle>
+          <SelectBox
+            options={schemaList?.map(data => data.schemaName) || ["Preset"]}
+            defaultSelect={schemaList ? schemaList[0].schemaName : "Preset"}
+            onSelectionChange={(_, index) => setCurrentSchemaIndex(index)}
+          />
+          <p className={css.fieldSubText}>
+            Choose your schema type for content field.
+          </p>
+        </div>
+        {schemaProperty.map(([property, data]) => (
+          <div>
+            <div className={css.fieldHeader}>
+              <FieldTitle>{property}</FieldTitle>
+              <div className={css.fieldTypeWrapper}>
+                <div className={css.fieldType}>{data.type}</div>
+                {schema.required && schema.required.includes(property) && (
+                  <div className={css.fieldType}>required</div>
+                )}
+              </div>
+            </div>
+            <div>{contentsError[property]}</div>
+            {editorForType[data.type] === "textfield" && (
+              <TextField
+                value={content[property] as string}
+                onTextFieldChange={text => {
+                  data.type === "number"
+                    ? setContents<number>({
+                        name: property,
+                        content: parseInt(text),
+                        schema,
+                      })
+                    : setContents<string>({
+                        name: property,
+                        content: text,
+                        schema,
+                      });
+                }}
+                placeholder={property}
+              />
+            )}
+            {editorForType[data.type] === "dateInput" && (
+              <TextField
+                type="date"
+                value={dayjs().format("YYYY-MM-DD")}
+                onTextFieldChange={text => {
+                  data.type === "number"
+                    ? setContents<number>({
+                        name: property,
+                        content: parseInt(text),
+                        schema,
+                      })
+                    : setContents<string>({
+                        name: property,
+                        content: text,
+                        schema,
+                      });
+                }}
+                placeholder={property}
+              />
+            )}
+            {editorForType[data.type] === "textarea" && (
+              <textarea
+                className={css.textarea}
+                value={content[property] as string}
+                onChange={e =>
+                  setContents<typeof data.type>({
+                    name: property,
+                    content: e.target.value,
+                    schema,
+                  })
+                }
+              />
+            )}
+          </div>
+        ))}
+      </section>
+      <footer className={css.footer}>
+        <button onClick={handleClickSave} className={css.saveButton}>
+          Save
+        </button>
+      </footer>
+    </div>
+  );
+}
---
+++ src/ProjectContents/ProjectContentsList.tsx
@@ -0,0 +1,164 @@
+import { Navigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
+
+import {
+  useCheckboxState,
+  useSetCheckboxState,
+} from "../@shared/useCheckboxStore";
+import { useModal } from "../@shared";
+import { ModalSchemaProperties } from "../ProjectSchema/ModalSchemaProperties";
+import { useContentsListQuery } from "./useContentsQuery";
+import { useDeleteContentsMutation } from "./useContentsMutation";
+import { sortByMode as sortBy } from "../@utils/sortByMode";
+import * as css from "./index.css";
+
+import { JsonSchema } from "../@packages/type";
+import { Contents, SchemaList } from "../@types/api";
+import type { SortMode, OrderMode } from "../@types/cms";
+
+import { mockContentList } from "../test/mock/schema_mock";
+
+
+type Options = {
+  schema: JsonSchema;
+  token: string;
+  orderOption: OrderMode;
+  sortOption: SortMode;
+  searchword: string;
+};
+
+// TODO type
+export function ProjectContentsList({
+  schema,
+  token,
+  orderOption,
+  sortOption,
+  searchword,
+}: Options) {
+  const { openModal } = useModal();
+  const { values: checkedContentsId, isAllChecked } = useCheckboxState();
+  const { toggleAllChecked, setValue: setCheckboxValue } =
+    useSetCheckboxState();
+  const { data: contentsList, refetch } = useContentsListQuery({
+    schemaName: schema.title,
+    token,
+    page: 1,
+    sort: sortOption,
+    order: orderOption,
+  });
+
+  if (!contentsList) {
+    return <Navigate to="/" />;
+  }
+
+  // const contentsList = mockContentList;
+
+  const queryClient = useQueryClient();
+  const deleteContents = useDeleteContentsMutation({
+    onSuccess: () => {
+      alert("Success contents delete");
+      // queryClient.invalidateQueries({ queryKey: ["contents"] });
+      // refetch();
+    },
+    onError: () => {
+      alert("Failed to delete contents. Please try again.");
+    },
+  });
+
+  const handleSchemaClick = ({ index }: { index: number }) => {
+    // openModal({
+    //   component: (
+    //     <ModalSchemaProperties
+    //       currentSchema={schema}
+    //       projectName={projectName}
+    //     />
+    //   ),
+    // });
+  };
+
+  const handleDelete = ({ contentIds }: { contentIds: string[] }) => {
+    if (window.confirm("Do you want to delete the field?")) {
+      deleteContents.mutate({
+        token,
+        schemaName: schema.title,
+        contentIds,
+      });
+    }
+  };
+
+  return (
+    <>
+      {!!checkedContentsId.size && (
+        <div className={css.selectOptionField}>
+          <div>{`${
+            isAllChecked ? mockContentList.length : checkedContentsId.size
+          } selected`}</div>
+          <BsFillTrashFill
+            onClick={() => handleDelete({ contentIds: [...checkedContentsId] })}
+            className={css.optionIcon}
+          />
+        </div>
+      )}
+      <table className={css.table}>
+        <thead>
+          <tr>
+            <th className={css.thCheckbox}>
+              <input
+                type="checkbox"
+                value="checkbox-parent"
+                checked={isAllChecked}
+                // TODO
+                onChange={() =>
+                  toggleAllChecked(contentsList.map(data => data._id))
+                }
+              />
+            </th>
+            <th className={css.th}>Id</th>
+            <th className={css.th}>Schema Type</th>
+            <th className={css.th}>Created At</th>
+            <th className={css.th}>Updated At</th>
+          </tr>
+        </thead>
+        <tbody className={css.tbody}>
+          {sortBy<any>({
+            mode: orderOption,
+            data: contentsList,
+            fieldName: "schemaName",
+          })
+            // TODO filter
+            // .filter((data: SchemaList) =>
+            //   searchword ? data.schema.title.includes(searchword) : true,
+            // )
+            .map((data: Contents, index: number) => (
+              <tr className={css.row} key={data._id}>
+                <td className={css.cell}>
+                  <div className={css.checkboxField}>
+                    <input
+                      type="checkbox"
+                      value={data._id}
+                      checked={isAllChecked || checkedContentsId.has(data._id)}
+                      onChange={e => setCheckboxValue(e.target.value)}
+                    />
+                  </div>
+                </td>
+                <td className={css.cell}>
+                  <div
+                    onClick={() => handleSchemaClick({ index })}
+                    className={css.nameField}
+                  >
+                    <span>{data._id}</span>
+                  </div>
+                </td>
+                <td className={css.cell}>
+                  <div className={css.typeField}>{schema.title}</div>
+                </td>
+                <td className={css.cell}>04/18/2023 7:19 AM</td>
+                <td className={css.cell}>04/18/2023 7:19 AM</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
