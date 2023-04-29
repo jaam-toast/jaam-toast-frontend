20b37087d9f52bc99cfb208181a2070ff68f7c26
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-04-29 13:50:58 +0900

    ✨ feat: create contents info component (separate project dashboard)

--- src/ProjectDashboard/index.tsx
+++ src/ProjectDashboard/index.tsx
@@ -11,12 +11,13 @@ import { Preview, PreviewSkeleton } from "../ProjectDeploy/Preview";
 import { useProjectQuery } from "./useProjectQuery";
 import { COLORS } from "../@config/colors";
 import * as css from "./index.css";
+import { ContentsInfo } from "./ContentsInfo";
 
 export function ProjectDashboard() {
   const params = useParams();
   const { userName, projectName } = params;
 
-  if (!projectName) {
+  if (!projectName || !userName) {
     return <Navigate to="/error" />;
   }
 
@@ -71,62 +72,11 @@ export function ProjectDashboard() {
               </ul>
             </section>
           </div>
-          {project?.schemaList?.length ? (
-            <section className={css.cmsInfoSection}>
-              <span className={css.infoFieldTitle}>Api Info</span>
-              <ul className={css.cmsInfoList}>
-                <Link to={`/${userName}/${projectName}/schema`}>
-                  <li className={css.cmsInfo}>
-                    <div className={css.cmsInfoLeft}>
-                      <ColorBox>
-                        <BsColumnsGap color={COLORS.LAVENDAR_DARK} />
-                      </ColorBox>
-                      <span className={css.infoFieldTitle}>Schema</span>
-                    </div>
-                    <p className={css.infoText}>{project.schemaList.length}</p>
-                  </li>
-                </Link>
-                <Link to={`/${userName}/${projectName}/contents`}>
-                  <li className={css.cmsInfo}>
-                    <div className={css.cmsInfoLeft}>
-                      <ColorBox>
-                        <BsFileEarmarkImage color={COLORS.LAVENDAR_DARK} />
-                      </ColorBox>
-                      <span className={css.infoFieldTitle}>Assets</span>
-                    </div>
-                    <p className={css.infoText}>10</p>
-                  </li>
-                </Link>
-                <Link to={`/${userName}/${projectName}/assets`}>
-                  <li className={css.cmsInfo}>
-                    <div className={css.cmsInfoLeft}>
-                      <ColorBox>
-                        <BsStack color={COLORS.LAVENDAR_DARK} />
-                      </ColorBox>
-                      <span className={css.infoFieldTitle}>Contents</span>
-                    </div>
-                    <p className={css.infoText}>5</p>
-                  </li>
-                </Link>
-              </ul>
-            </section>
-          ) : (
-            <section className={css.emptyCmsSection}>
-              <span className={css.infoText}>
-                No contents here. <br />
-                <strong>Let's create some!</strong> Ready to start?
-              </span>
-              <div className={css.emptyCmsButtonWrapper}>
-                <button className={css.emptyCmsButton}>Quick Start</button>
-                <Link
-                  to={`/${userName}/${projectName}/schema`}
-                  className={css.emptyCmsButton}
-                >
-                  Add Schema
-                </Link>
-              </div>
-            </section>
-          )}
+          <ContentsInfo
+            schemaListCount={project?.schemaList?.length || 0}
+            userName={userName}
+            projectName={projectName}
+          />
         </div>
       </section>
     </div>
