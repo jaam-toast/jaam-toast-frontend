292fd4f16d7be6488187cf6de008e377d3cbef52
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 22:19:33 +0900

    ✨ feat: create content info page

---
+++ src/ContentInfo/index.css.ts
@@ -0,0 +1,65 @@
+import { style } from "@vanilla-extract/css";
+import { pretendard } from "../@styles/font.css";
+import { s } from "../@styles";
+import { COLORS } from "../@config/colors";
+
+export const container = style([s.fullWidth, s.fullHeigth, { flex: 1 }]);
+
+export const wrapper = style([
+  s.flexColumn,
+  s.border,
+  {
+    padding: "2rem",
+    backgroundColor: COLORS.WHITE,
+  },
+]);
+
+export const buttonWrapper = style([
+  s.flex,
+  s.flexSpaceBetween,
+  s.fullWidth,
+  {
+    paddingBottom: "1.5rem",
+  },
+]);
+
+export const header = style([
+  s.flex,
+  s.fullWidth,
+  {
+    justifyContent: "space-between",
+    position: "sticky",
+    zIndex: 10,
+    top: 0,
+    left: 0,
+    padding: "2rem 0",
+    backgroundColor: COLORS.WHITE,
+    fontWeight: 400,
+  },
+]);
+
+export const titleWrapper = style([s.flex, s.flexSpaceBetween, {}]);
+
+export const timeWrapper = style([s.flexColumn]);
+
+export const button = style([s.button, s.buttonSize.medium]);
+
+export const prevButton = style([
+  s.buttonColor.light,
+  {
+    ":hover": {
+      backgroundColor: COLORS.STRAWBERRY,
+      borderColor: "none",
+    },
+  },
+]);
+
+export const saveButton = style([
+  s.buttonColor.dark,
+  {
+    ":hover": {
+      backgroundColor: COLORS.STRAWBERRY,
+      borderColor: "none",
+    },
+  },
+]);
---
+++ src/ContentInfo/index.tsx
@@ -0,0 +1,101 @@
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+
+import { jsonSchemaValidator } from "@jaam-schema/src";
+
+import { useProjectQuery } from "../@shared/useProjectQuery";
+import { ContentPropertyList } from "./ContentPropertyList";
+import { TimeInfo } from "./TimeInfo";
+import {
+  useContentsState,
+  useSetContentsState,
+} from "../ProjectContents/useContentsStore";
+import { useUpdateContentMutation } from "../ProjectContents/useContentsMutation";
+import * as css from "./index.css";
+
+export function ContentInfo() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const { projectName, schemaName, contentId } = params;
+  const { content: contentState } = useContentsState();
+  const { reset } = useSetContentsState();
+
+  if (!projectName || !contentId || !schemaName) {
+    return <Navigate to="/error" />;
+  }
+
+  const { data: project } = useProjectQuery(projectName);
+
+  if (!project) {
+    return <Navigate to="/error" />;
+  }
+
+  const { schemaList, storageKey: token } = project;
+  const schema = useMemo(
+    () => schemaList.filter(data => data.schemaName === schemaName)[0].schema,
+    [],
+  );
+
+  const updateContent = useUpdateContentMutation({
+    onSuccess: () => {
+      alert("Success content update");
+      reset();
+      navigate(-1);
+    },
+    onError: e => {
+      console.log(e);
+      alert("Failed to update content. Please try again.");
+    },
+  });
+
+  const handlePrevBtnClick = () => {
+    navigate(-1);
+  };
+
+  const handleSaveBtnClick = () => {
+    const { result, message } = jsonSchemaValidator({
+      schema,
+      content: contentState,
+    });
+
+    if (!result) {
+      return alert(message);
+    }
+
+    updateContent.mutate({ token, schemaName, contentId });
+  };
+
+  return (
+    <div className={css.container}>
+      <div className={css.buttonWrapper}>
+        <button
+          onClick={handlePrevBtnClick}
+          className={`${css.button} ${css.prevButton}`}
+        >
+          + Prev
+        </button>
+        <button
+          onClick={handleSaveBtnClick}
+          className={`${css.button} ${css.saveButton}`}
+        >
+          + Save
+        </button>
+      </div>
+      <section className={css.wrapper}>
+        <header className={css.header}>
+          <div className={css.titleWrapper}>
+            <h3>Content Info</h3>
+          </div>
+          <div className={css.timeWrapper}>
+            <TimeInfo schema={schema} token={token} contentId={contentId} />
+          </div>
+        </header>
+        <ContentPropertyList
+          schema={schema}
+          token={token}
+          contentId={contentId}
+        />
+      </section>
+    </div>
+  );
+}
