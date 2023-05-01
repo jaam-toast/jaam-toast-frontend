d243a9daaf2ce97ddeb209c53268a0c553cf6a72
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 21:18:47 +0900

    🔥 chore: move to shared

---
+++ src/@shared/useProjectQuery.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { useAuth } from "../@shared/useAuth";
+import APIClient from "../@utils/api";
+
+export function useProjectQuery(projectName: string) {
+  const { user } = useAuth();
+  const api = new APIClient()
+    .setUserId(user?.id)
+    .setAccessToken(user?.accessToken)
+    .setGithubAccessToken(user?.githubAccessToken);
+
+  return useQuery({
+    queryKey: ["project", projectName],
+    queryFn: () => api.getProject(projectName),
+  });
+}
