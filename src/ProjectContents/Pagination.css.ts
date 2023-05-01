e67ddb2c1f279c375bed208e1a919db8c6975510
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 22:26:32 +0900

    ✨ feat: create pagination component

---
+++ src/ProjectContents/Pagination.css.ts
@@ -0,0 +1,52 @@
+import { style } from "@vanilla-extract/css";
+import { s } from "../@styles";
+import { COLORS } from "../@config/colors";
+
+export const hidden = style({ visibility: "hidden" });
+
+export const paginationContainer = style([
+  s.flex,
+  s.alignItemsCenter,
+  s.justifyContentCenter,
+  {
+    height: "30%",
+  },
+]);
+
+export const paginationNumberWrapper = style([
+  s.flex,
+  s.justifyContentCenter,
+  s.alignItemsCenter,
+  {
+    minWidth: "12rem",
+    maxWidth: "12rem",
+  },
+]);
+
+export const paginationNumberPoint = style({ color: COLORS.LAVENDAR_DARK });
+
+export const paginationSideWrapper = style([
+  s.flex,
+  s.flexSpaceBetween,
+  s.alignItemsCenter,
+  {
+    transition: "left 250ms linear",
+  },
+]);
+
+export const paginationArrow = style([s.pointer]);
+
+export const paginationCell = style([
+  s.inlineFlex,
+  s.justifyContentCenter,
+  s.pointer,
+  {
+    padding: "0.3rem 0.5rem",
+    minWidth: "2rem",
+    maxWidth: "2rem",
+    fontSize: "0.9rem",
+    ":hover": {
+      color: COLORS.LAVENDER,
+    },
+  },
+]);
---
+++ src/ProjectContents/Pagination.tsx
@@ -0,0 +1,91 @@
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+
+import * as css from "./Pagination.css";
+
+type PaginationProps = {
+  page: number;
+  pageLimit?: number;
+  contentsLimit?: number;
+  contentsCount: number;
+  onClickPage: (page: number) => void;
+};
+
+export function Pagination({
+  page,
+  pageLimit = 5,
+  contentsLimit = 20,
+  contentsCount,
+  onClickPage,
+}: PaginationProps) {
+  const totalPageCount = Math.ceil(contentsCount / contentsLimit);
+  const offset = (Math.ceil(page / pageLimit) - 1) * pageLimit;
+
+  const handleNextPageClick = () => {
+    page + pageLimit > totalPageCount
+      ? onClickPage(totalPageCount)
+      : onClickPage(page + pageLimit);
+  };
+
+  const handlePrevPageClick = () => {
+    page - pageLimit < 1 ? onClickPage(1) : onClickPage(page - pageLimit);
+  };
+
+  const handlePageNumberClick = (page: number) => {
+    onClickPage(page);
+  };
+
+  return (
+    <div className={css.paginationContainer}>
+      <div
+        className={`${css.paginationSideWrapper} ${
+          page !== 1 ? "" : css.hidden
+        }`}
+      >
+        <BsArrowLeftShort
+          onClick={handlePrevPageClick}
+          size={25}
+          className={`${css.paginationArrow} ${css.paginationCell}`}
+        />
+        <span
+          onClick={() => handlePageNumberClick(1)}
+          className={css.paginationCell}
+        >
+          1
+        </span>
+        <span className={`${css.paginationCell}`}>・・・</span>
+      </div>
+      <div className={css.paginationNumberWrapper}>
+        {[...new Array(pageLimit)]
+          .filter((_, index) => offset + index < totalPageCount)
+          .map((_, index) => (
+            <span
+              onClick={() => handlePageNumberClick(index + offset + 1)}
+              className={`${css.paginationCell} ${
+                index + offset + 1 === page ? css.paginationNumberPoint : ""
+              }`}
+            >
+              {index + offset + 1}
+            </span>
+          ))}
+      </div>
+      <div
+        className={`${css.paginationSideWrapper}  ${
+          page !== totalPageCount ? "" : css.hidden
+        }`}
+      >
+        <span className={`${css.paginationCell}`}>・・・</span>
+        <span
+          onClick={() => handlePageNumberClick(totalPageCount)}
+          className={css.paginationCell}
+        >
+          {totalPageCount}
+        </span>
+        <BsArrowRightShort
+          onClick={handleNextPageClick}
+          size={25}
+          className={`${css.paginationArrow} ${css.paginationCell}`}
+        />
+      </div>
+    </div>
+  );
+}
