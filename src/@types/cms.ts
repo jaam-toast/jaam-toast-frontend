0cb45c6317b510a198a08520704a6cde303fd103
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 21:27:44 +0900

    ✨ feat: create default property value record

--- src/@types/cms.ts
+++ src/@types/cms.ts
@@ -1,3 +1,20 @@
+import dayjs from "dayjs";
+
+import type { JaamSchemaPropertyType } from "@jaam-schema/src";
+
+export const DEFAULT_VALUE_FOR_TYPE: Record<
+  JaamSchemaPropertyType,
+  string | number | boolean
+> = {
+  text: "",
+  textarea: "",
+  email: "",
+  link: "",
+  date: dayjs().format("YYYY-MM-DD"),
+  number: 0,
+  boolean: false,
+};
+
 export type SortMode = "createdAt" | "updatedAt";
 
 export type OrderMode = "ascending" | "descending";
