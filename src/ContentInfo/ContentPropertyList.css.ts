8f7e9811050a52b698ed3d54768e120afacee5fe
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 22:17:17 +0900

    ✨ feat: create content prooperty list

---
+++ src/ContentInfo/ContentPropertyList.css.ts
@@ -0,0 +1,20 @@
+import { style } from "@vanilla-extract/css";
+import { s } from "../@styles";
+import { COLORS } from "../@config/colors";
+
+export const schemaNameSection = style([s.flexColumn, { gap: "1rem" }]);
+
+export const fieldHeader = style([
+  s.flex,
+  s.flexSpaceBetween,
+  s.alignItemsCenter,
+]);
+
+export const fieldTypeWrapper = style([s.flex, { gap: "1rem" }]);
+
+export const fieldType = style({
+  padding: "0.2rem 0.3rem",
+  borderRadius: "0.5rem",
+  backgroundColor: COLORS.GREY_CLEAR,
+  color: COLORS.WHITE,
+});
