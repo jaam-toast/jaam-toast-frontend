5d115a7f8bb57f0f5cd83be253222d156a179132
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-01 22:29:58 +0900

    ✨ feat: create  content query, contents query

---
+++ src/ProjectContents/useContentQuery.tsx
@@ -0,0 +1,20 @@
+import { useQuery, useQueries } from "@tanstack/react-query";
+
+import { ContentsAPIClient } from "../@utils/contentsAPI";
+
+export function useContentQuery({
+  schemaName,
+  token,
+  contentId,
+}: {
+  schemaName: string;
+  token: string;
+  contentId?: string;
+}) {
+  const contentsAPI = new ContentsAPIClient().setToken(token);
+
+  return useQuery({
+    queryKey: ["content", schemaName, contentId],
+    queryFn: () => contentsAPI.getContent({ schemaName, contentId }),
+  });
+}
