305001f3075467dd5436df9dcf6b1cff5f2b08a4
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-09 03:15:22 +0900

    ✨ feat: add user data query

---
+++ src/@hooks/useUserDataQuery.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { useAuth } from "./useAuth";
+import APIClient from "../@utils/api";
+
+export function useUserDataQuery() {
+  const { user } = useAuth();
+  const api = new APIClient()
+    .setUserId(user?.id)
+    .setAccessToken(user?.accessToken)
+    .setGithubAccessToken(user?.githubAccessToken);
+
+  return useQuery({
+    queryKey: ["user-data", user.id],
+    queryFn: () => api.getUserData(user.id),
+  });
+}
