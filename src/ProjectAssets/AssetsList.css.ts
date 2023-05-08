bdb796a1fdc73d878ca719c7ae67006cb4bb39b4
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-09 00:12:58 +0900

    ✨ feat: create assets list component

---
+++ src/ProjectAssets/AssetsList.css.ts
@@ -0,0 +1,80 @@
+import { style } from "@vanilla-extract/css";
+import { s } from "../@styles";
+import { COLORS } from "../@config/colors";
+
+export const container = style([
+  s.flex,
+  s.full,
+  {
+    borderTop: `1px solid ${COLORS.GREY_CLEAR}`,
+    paddingTop: "1.5rem",
+    gap: "0.5rem",
+    display: "flex",
+    flexWrap: "wrap",
+  },
+]);
+
+const assetPreviewBase = style([
+  {
+    borderRadius: "0.5rem",
+    width: "10rem",
+    height: "10rem",
+  },
+]);
+
+export const assetPreviewWrapper = style([
+  assetPreviewBase,
+  s.pointer,
+  {
+    position: "relative",
+  },
+]);
+
+export const assetPreviewName = style([s.inlineFlex, s.flexColumn]);
+
+const assetPreviewInfoBase = style([
+  assetPreviewBase,
+  {
+    position: "absolute",
+    zIndex: 9,
+  },
+]);
+
+export const assetPreviewInfo = style([
+  assetPreviewInfoBase,
+  s.flexColumn,
+  s.justifyContentCenter,
+  s.alignItemsCenter,
+  {
+    gap: "1rem",
+    zIndex: 99,
+    color: COLORS.WHITE,
+    overflowX: "scroll",
+    textOverflow: "ellipsis",
+    textAlign: "left",
+    fontSize: "0.8rem",
+    padding: "0.5rem",
+  },
+]);
+
+export const assetPreviewBg = style([
+  assetPreviewInfoBase,
+  { backgroundColor: COLORS.BLACK, opacity: "70%" },
+]);
+
+export const assetPreviewImgWrapper = style([
+  assetPreviewBase,
+  {
+    position: "absolute",
+  },
+]);
+
+export const assetPreviewImg = style([
+  s.pointer,
+  {
+    borderRadius: "0.5rem",
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
+  },
+]);
---
+++ src/ProjectAssets/AssetsList.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+import { useModal } from "../@hooks";
+import { AssetAPIClient } from "../@utils/assetsAPI";
+import { ModalAssetInfo } from "./ModalAssetInfo";
+import * as css from "./AssetsList.css";
+
+type AssetInfo = {
+  url?: string;
+  name?: string;
+  size?: number;
+};
+
+export function AssetsList({
+  projectName,
+  token,
+}: {
+  projectName: string;
+  token: string;
+}) {
+  const [assetsInfoList, setAssetsInfoList] = useState<AssetInfo[]>([]);
+  const [onMouseImgIndex, setOnMouseImgIndex] = useState<number | null>(null);
+  const { openModal } = useModal();
+
+  useEffect(() => {
+    const assetAPI = new AssetAPIClient();
+    assetAPI.connect();
+
+    (async () => {
+      const assetList = await assetAPI.getAssetInfoList(projectName);
+
+      setAssetsInfoList(assetList);
+    })();
+  }, []);
+
+  const handleAseetClick = (asset: AssetInfo) => {
+    openModal({ component: <ModalAssetInfo asset={asset} token={token} /> });
+  };
+
+  return (
+    <section className={css.container}>
+      {assetsInfoList.map((asset, index) => (
+        <div
+          key={asset.url}
+          className={css.assetPreviewWrapper}
+          onMouseEnter={() => setOnMouseImgIndex(index)}
+          onMouseLeave={() => setOnMouseImgIndex(null)}
+          onClick={() => handleAseetClick(asset)}
+        >
+          {index === onMouseImgIndex && (
+            <>
+              <div className={css.assetPreviewInfo}>
+                <span className={css.assetPreviewName}>
+                  {asset.name} <br />
+                </span>
+                <span>{asset.size && Math.round(asset.size / 1024)}kb</span>
+              </div>
+              <div className={css.assetPreviewBg} />
+            </>
+          )}
+          <div className={css.assetPreviewImgWrapper}>
+            <img className={css.assetPreviewImg} src={asset.url} alt="k" />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
