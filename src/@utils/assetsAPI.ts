9b8417256391d0983b96be27ce660bcd8450718a
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-16 18:34:24 +0900

    ➖ chore: delete aws-amplify

--- src/@utils/assetsAPI.ts
+++ src/@utils/assetsAPI.ts
@@ -1,4 +1,4 @@
-import { Amplify, Storage } from "aws-amplify";
+// import { Amplify, Storage } from "aws-amplify";
 // import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 // import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 import { nanoid } from "nanoid";
@@ -22,18 +22,18 @@ export class AssetAPIClient {
   // }
 
   connect() {
-    Amplify.configure({
-      Auth: {
-        identityPoolId: Config.POOL_ID,
-        region: Config.REGION,
-      },
-      Storage: {
-        AWSS3: {
-          bucket: Config.BUCKET_NAME,
-          region: Config.REGION,
-        },
-      },
-    });
+    // Amplify.configure({
+    //   Auth: {
+    //     identityPoolId: Config.POOL_ID,
+    //     region: Config.REGION,
+    //   },
+    //   Storage: {
+    //     AWSS3: {
+    //       bucket: Config.BUCKET_NAME,
+    //       region: Config.REGION,
+    //     },
+    //   },
+    // });
   }
 
   async isDuplicatedName({
@@ -44,9 +44,8 @@ export class AssetAPIClient {
     name: string;
   }) {
     try {
-      const assetsList = await this.getAssetInfoList(folderName);
-
-      return assetsList.some(asset => asset.path === `${folderName}/${name}`);
+      // const assetsList = await this.getAssetInfoList(folderName);
+      // return assetsList.some(asset => asset.path === `${folderName}/${name}`);
     } catch (error) {
       throw error;
     }
@@ -60,32 +59,29 @@ export class AssetAPIClient {
     //   Key: key,
     // });
     // const response = await this.client.send(command);
-
     // console.log({ response });
-    return Storage.get(key);
+    // return Storage.get(key);
     // } catch (error) {}
   }
 
-  async getAssetInfoList(folderName: string): Promise<Asset[]> {
+  async getAssetInfoList(folderName: string) {
     try {
-      const infoList = await Storage.list(folderName);
-
-      return Promise.all(
-        infoList.results.map(async asset => {
-          const url =
-            asset &&
-            asset.key &&
-            (await Storage.get(asset.key, {
-              expires: 60 * 60 * 24 * 365,
-            }));
-
-          return {
-            url,
-            path: asset.key,
-            size: asset.size,
-          };
-        }),
-      );
+      // const infoList = await Storage.list(folderName);
+      // return Promise.all(
+      //   infoList.results.map(async asset => {
+      //     const url =
+      //       asset &&
+      //       asset.key &&
+      //       (await Storage.get(asset.key, {
+      //         expires: 60 * 60 * 24 * 365,
+      //       }));
+      //     return {
+      //       url,
+      //       path: asset.key,
+      //       size: asset.size,
+      //     };
+      //   }),
+      // );
     } catch (error) {
       throw error;
     }
@@ -99,22 +95,19 @@ export class AssetAPIClient {
     name: string;
     folderName: string;
     asset: File;
-  }): Promise<Asset | null> {
+  }) {
     try {
-      const isDuplicatedName = await this.isDuplicatedName({
-        folderName,
-        name,
-      });
-
-      const { key: path } = await Storage.put(
-        `${folderName}/${isDuplicatedName ? name : nanoid() + name}`,
-        asset,
-      );
-
-      const assetsList = await this.getAssetInfoList(path);
-      const assetData = assetsList.filter(asset => asset.path === path)[0];
-
-      return assetData;
+      // const isDuplicatedName = await this.isDuplicatedName({
+      //   folderName,
+      //   name,
+      // });
+      // const { key: path } = await Storage.put(
+      //   `${folderName}/${isDuplicatedName ? name : nanoid() + name}`,
+      //   asset,
+      // );
+      // const assetsList = await this.getAssetInfoList(path);
+      // const assetData = assetsList.filter(asset => asset.path === path)[0];
+      // return assetData;
     } catch (error) {
       return null;
     }
@@ -122,9 +115,8 @@ export class AssetAPIClient {
 
   async deleteAsset({ path }: { path: string }) {
     try {
-      const result = await Storage.remove(path);
-
-      return result;
+      // const result = await Storage.remove(path);
+      // return result;
     } catch (error) {
       return null;
     }
