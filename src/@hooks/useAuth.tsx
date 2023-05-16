bd8e224639c3977c1d1aa2af93ca72a69ff0e018
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-05-16 17:48:10 +0900

    ♻️ refactor: change user name to userId

--- src/@hooks/useAuth.tsx
+++ src/@hooks/useAuth.tsx
@@ -3,11 +3,11 @@ import Cookies from "js-cookie";
 import { logout } from "../@utils/api";
 
 export function useAuth() {
-  const user = Cookies.get("userId") || null;
+  const userId = Cookies.get("userId") || null;
 
   return {
-    user,
-    isLogin: !!user,
+    userId,
+    isLogin: !!userId,
     logout,
   };
 }
